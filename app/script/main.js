77e51c9a7adb11b87c1418b09def3ed558f357a3
Karim Abdelghany <132405453+karimabdelghanyhefnawy1@users.noreply.github.com>
2024-04-27 01:06:45 +0300

    click action

--- app/script/main.js
+++ app/script/main.js
@@ -15,6 +15,30 @@ theRest.onclick = function () {
   window.location.reload();
 }
 
+let rockIcon = document.querySelector(".hero__item--rock")
+let paperIcon = document.querySelector(".hero__item--paper")
+let scissorsIcon = document.querySelector(".hero__item--scissors")
+let triangleImage = document.querySelector(".triangle")
+
+rockIcon.onclick = function () {
+  paperIcon.style.display = 'none';
+  scissorsIcon.style.display = 'none';
+  triangleImage.style.display = 'none';
+}
+paperIcon.onclick = function () {
+  rockIcon.style.display = 'none';
+  scissorsIcon.style.display = 'none';
+  triangleImage.style.display = 'none';
+}
+scissorsIcon.onclick = function () {
+  paperIcon.style.display = 'none';
+  rockIcon.style.display = 'none';
+  triangleImage.style.display = 'none';
+}
+
+
+
+
 
 
 
