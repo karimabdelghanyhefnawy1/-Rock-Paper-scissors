331d62c3ada96d4d3a8056852009813dcae60b37
Karim Abdelghany <132405453+karimabdelghanyhefnawy1@users.noreply.github.com>
2024-04-29 10:03:50 +0300

    click action transform

--- app/script/main.js
+++ app/script/main.js
@@ -21,20 +21,32 @@ let scissorsIcon = document.querySelector(".hero__item--scissors")
 let triangleImage = document.querySelector(".triangle")
 
 rockIcon.onclick = function () {
+  rockIcon.style.cssText = "top: 40%; left: 10%; transform: translate(-50%, -50%); transition: all 0.0.0001s ease;";
   paperIcon.style.display = 'none';
   scissorsIcon.style.display = 'none';
   triangleImage.style.display = 'none';
 }
 paperIcon.onclick = function () {
+  paperIcon.style.cssText = "top: 40%; right: 60%; transform: translate(-50%, -50%); transition: all 0.0001s ease;";
   rockIcon.style.display = 'none';
   scissorsIcon.style.display = 'none';
   triangleImage.style.display = 'none';
 }
 scissorsIcon.onclick = function () {
+  scissorsIcon.style.cssText = "top: 40%; left: 10%; transform: translate(-50%, -50%); transition: all 0.0001s ease;";
   paperIcon.style.display = 'none';
   rockIcon.style.display = 'none';
   triangleImage.style.display = 'none';
 }
+
+
+
+
+
+
+
+
+
 
 
 
