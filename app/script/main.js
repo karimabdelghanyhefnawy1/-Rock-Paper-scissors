40eb616366f4027bb7d5510c65ffadc4e2a759f8
Karim Abdelghany <132405453+karimabdelghanyhefnawy1@users.noreply.github.com>
2024-05-03 11:43:24 +0300

    Random Choice

--- app/script/main.js
+++ app/script/main.js
@@ -15,29 +15,42 @@ theRest.onclick = function () {
   window.location.reload();
 }
 
+// let circles = document.querySelectorAll(".hero__item--paper, .hero__item--scissors");
 let rockIcon = document.querySelector(".hero__item--rock")
 let paperIcon = document.querySelector(".hero__item--paper")
 let scissorsIcon = document.querySelector(".hero__item--scissors")
 let triangleImage = document.querySelector(".triangle")
 
-rockIcon.onclick = function () {
-  rockIcon.style.cssText = "top: 40%; left: 10%; transform: translate(-50%, -50%); transition: all 0.0.0001s ease;";
-  paperIcon.style.display = 'none';
-  scissorsIcon.style.display = 'none';
+rockIcon.onclick = function hideRandomButton() {
+
+  let circles1 = document.querySelectorAll(".hero__item--paper, .hero__item--scissors");
+
+  let randomIndex = Math.floor(Math.random() * circles1.length);
+  let circleToHide1 = circles1[randomIndex];
+
+  circleToHide1.style.display = 'none';
   triangleImage.style.display = 'none';
 }
-paperIcon.onclick = function () {
-  paperIcon.style.cssText = "top: 40%; right: 60%; transform: translate(-50%, -50%); transition: all 0.0001s ease;";
-  rockIcon.style.display = 'none';
-  scissorsIcon.style.display = 'none';
+paperIcon.onclick = function hideRandomButton() {
+  let circles2 = document.querySelectorAll(".hero__item--rock, .hero__item--scissors");
+
+  let randomIndex = Math.floor(Math.random() * circles2.length);
+  let circleToHide2 = circles2[randomIndex];
+
+  circleToHide2.style.display = 'none';
   triangleImage.style.display = 'none';
 }
-scissorsIcon.onclick = function () {
-  scissorsIcon.style.cssText = "top: 40%; left: 10%; transform: translate(-50%, -50%); transition: all 0.0001s ease;";
-  paperIcon.style.display = 'none';
-  rockIcon.style.display = 'none';
+scissorsIcon.onclick = function hideRandomButton() {
+  let circles3 = document.querySelectorAll(".hero__item--rock, .hero__item--paper");
+
+  let randomIndex = Math.floor(Math.random() * circles3.length);
+  let  circleToHide3 = circles3[randomIndex];
+
+  circleToHide3.style.display = 'none';
   triangleImage.style.display = 'none';
 }
+
+
 
 
 
